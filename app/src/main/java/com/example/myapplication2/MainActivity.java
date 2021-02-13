19c7f87d1d58f62f1faa8c0711d1720009bd19d8
Matthew Molloy <whamtet@gmail.com>
2021-02-13 18:51:20 +0700

    init commit

---
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication2;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.webkit.WebViewAssetLoader;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView myWebView = new WebView(this.getApplicationContext());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+
+        // add implementation "androidx.webkit:webkit:1.2.0" to build.gradle
+        final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
+                .build();
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            @RequiresApi(21)
+            public WebResourceResponse shouldInterceptRequest(WebView view,
+                                                              WebResourceRequest request) {
+                return assetLoader.shouldInterceptRequest(request.getUrl());
+            }
+        });
+
+        setContentView(myWebView);
+
+        myWebView.loadUrl("https://appassets.androidplatform.net/assets/www/index.html");
+
+    }
+
+}
